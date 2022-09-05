d57e38b87042c67ec41042c3c7caae7f465439c1
wenjielin <1920361654@qq.com>
2022-09-05 21:20:00 +0800

    tinymitt init

--- src/tinyMitt.ts
+++ src/tinyMitt.ts
@@ -15,7 +15,7 @@ E.prototype = {
     once: function () {
 
     },
-    emit: function () {
+    emit: function (name) {
         /**
          * slice(start, end) 方法可从已有的数组中返回选定的元素，返回新数组。 start从何处开始
          * call（）和apply（）方法都是在特定的作用域中调用函数，实际上等于设置函数体内this对象的值
