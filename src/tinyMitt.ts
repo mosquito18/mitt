3bd746c6ee46e46a69ad118be556a9e7ef20d4e2
wenjielin <wenjielin@whale.im>
2022-09-05 09:33:37 +0800

    tinymitt init

--- src/tinyMitt.ts
+++ src/tinyMitt.ts
@@ -3,8 +3,14 @@ function E() {
 }
 
 E.prototype = {
-    on: function () {
+    on: function (name, callback, ctx) {
+        const e = this.e || (this.e = {});
 
+        (e[name] || (e[name] = [])).push({
+            fn: callback,
+            ctx
+        })
+        return this
     },
     once: function () {
 
